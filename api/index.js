b90cc9017eb3d59b2afbdd1e86a5bda43c04d354
unknown <shkelzen.dunisha@umsh.edu.al>
2023-12-04 17:38:41 -0500

    test controller and router api created

--- api/index.js
+++ api/index.js
@@ -1,13 +1,23 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-dotenv.config()
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from './routes/user.route.js'
+dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => { console.log('Connected to MongoDB') }).catch((err) => { console.log(err) })
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-const app = express()
+const app = express();
+
+app.use('/api/user', userRouter)
 
 app.listen(3000, () => {
-    console.log('Server running on port 3000!!')
-}
-)
+  console.log("Server running on port 3000!!");
+});
+
